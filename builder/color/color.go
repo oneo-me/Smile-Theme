74b544ac5ed7464bab41c8ef29d9b24e54911690
ONEO <oneo.me@outlook.com>
2018-02-22 22:46:04 +0800

    refactor: extend color utilities with HSBA and alpha blending

--- builder/color/color.go
+++ builder/color/color.go
@@ -1,83 +1,106 @@
 package color
 
-import (
-	"strings"
-)
-
-// RGBA 颜色
-type RGBA struct {
+// Color 颜色
+type Color struct {
 	// R 0 - 255
-	R int64
+	R float64
 	// G 0 - 255
-	G int64
+	G float64
 	// B 0 - 255
-	B int64
+	B float64
 	// A 0 - 100
 	A float64
 }
 
-// NewRGBA 初始化颜色
-func NewRGBA(r, g, b int64, a float64) *RGBA {
-	rgba := new(RGBA)
-	rgba.R = r
-	rgba.G = g
-	rgba.B = b
-	rgba.A = a
-	return rgba
+// HEX 初始化颜色
+func HEX(hex string) *Color {
+	r, g, b, a := hex2rgba(hex)
+	return &Color{r, g, b, a}
 }
 
-// NewHEX 初始化颜色
-func NewHEX(hex string) *RGBA {
-	rgba := new(RGBA)
-	switch len(hex) {
-	case 6, 8:
-		hex = "#" + hex
-	case 7, 9:
-		if strings.Index(hex, "#") != 0 {
-			panic("这个颜色值有猫饼")
-		}
-	default:
-		panic("这个颜色值有猫饼")
-	}
-	rgba.R = conv10(hex[1:3])
-	rgba.G = conv10(hex[3:5])
-	rgba.B = conv10(hex[5:7])
-	if len(hex) == 9 {
-		rgba.A = float64(conv10(hex[7:9])) / 2.55
-	} else {
-		rgba.A = 100
-	}
-	return rgba
+// RGBA 初始化颜色
+func RGBA(r, g, b, a float64) *Color {
+	return &Color{r, g, b, a}
+}
+
+// HSBA 初始化颜色
+func HSBA(h, s, hb, a float64) *Color {
+	r, g, b := hsb2rgb(h, s, hb)
+	return &Color{r, g, b, a}
 }
 
 // HEX 十六进制颜色
-func (rgba *RGBA) HEX() string {
-	r := "#"
-	r += conv16(rgba.R)
-	r += conv16(rgba.G)
-	r += conv16(rgba.B)
-	r += conv16(int64(rgba.A * 2.55))
-	return strings.ToUpper(r)
+func (color *Color) HEX() string {
+	return rgba2hex(color.R, color.G, color.B, color.A)
 }
 
-// Add 添加颜色
-func (rgba *RGBA) Add(color *RGBA) *RGBA {
-	r1 := float64(rgba.R)
-	g1 := float64(rgba.G)
-	b1 := float64(rgba.B)
-	a1 := rgba.A / 100
-	r2 := float64(color.R)
-	g2 := float64(color.G)
-	b2 := float64(color.B)
+// Brightness 不考虑透明度获取亮度 0 - 100
+func (color *Color) Brightness() float64 {
+	return (0.2126*color.R + 0.7152*color.G + 0.0722*color.B) / 2.55
+}
+
+// AlphaBlend 透明度混合
+func (color *Color) AlphaBlend(color2 *Color) *Color {
+	// 前景
+	r1 := color2.R
+	g1 := color2.G
+	b1 := color2.B
+	a1 := color2.A / 100
+
+	// 背景
+	r2 := color.R
+	g2 := color.G
+	b2 := color.B
 	a2 := color.A / 100
 
+	// 合并
 	r := r1*a1 + r2*a2*(1-a1)
 	g := g1*a1 + g2*a2*(1-a1)
 	b := b1*a1 + b2*a2*(1-a1)
 	a := 1 - (1-a1)*(1-a2)
+
+	// 合并
 	r = r / a
 	g = g / a
 	b = b / a
 
-	return NewRGBA(int64(r), int64(g), int64(b), a*100)
+	return &Color{r, g, b, a * 100}
+}
+
+// Opacity 修改不透明度
+func (color *Color) Opacity(a float64) *Color {
+	return &Color{color.R, color.G, color.B, a}
+}
+
+// Lighten 增亮，百分比
+func (color *Color) Lighten(amount float64) *Color {
+	h, s, hb := rgb2hsb(color.R, color.G, color.B)
+	hb *= 1 + amount
+	if hb > 1 {
+		hb = 1
+	} else if hb < 0 {
+		hb = 0
+	}
+	r, g, b := hsb2rgb(h, s, hb)
+	return &Color{r, g, b, color.A}
+}
+
+// Darken 变暗，百分比
+func (color *Color) Darken(amount float64) *Color {
+	h, s, hb := rgb2hsb(color.R, color.G, color.B)
+	hb *= 1 - amount
+	if hb > 1 {
+		hb = 1
+	} else if hb < 0 {
+		hb = 0
+	}
+	r, g, b := hsb2rgb(h, s, hb)
+	return &Color{r, g, b, color.A}
+}
+
+// Hue 色相 0 - 360
+func (color *Color) Hue(h float64) *Color {
+	_, s, hb := rgb2hsb(color.R, color.G, color.B)
+	r, g, b := hsb2rgb(h, s, hb)
+	return &Color{r, g, b, color.A}
 }
--- builder/color/utils.go
+++ builder/color/utils.go
@@ -1,24 +1,113 @@
 package color
 
 import (
+	"sort"
 	"strconv"
+	"strings"
 )
 
-func conv16(i int64) string {
-	r := strconv.FormatInt(i, 16)
+func to16(f float64) string {
+	r := strings.ToUpper(strconv.FormatInt(int64(f), 16))
 	if len(r) == 1 {
 		return "0" + r
 	}
 	if len(r) > 2 {
-		panic("这颜色值有问题")
+		panic(r + " 无法转换到十六进制")
 	}
 	return r
 }
 
-func conv10(str string) int64 {
+func to10(str string) float64 {
 	r, err := strconv.ParseInt(str, 16, 10)
 	if err != nil {
-		panic(err)
+		panic(str + " 无法转换到十进制")
 	}
-	return r
+	return float64(r)
+}
+
+func hex2rgba(hex string) (r, g, b, a float64) {
+	hex = strings.Replace(strings.TrimSpace(hex), "#", "", -1)
+	r = to10(hex[0:2])
+	g = to10(hex[2:4])
+	b = to10(hex[4:6])
+	a = 100
+	if len(hex) == 8 {
+		a = to10(hex[6:8]) / 2.55
+	}
+	return
+}
+
+func rgba2hex(r, g, b, a float64) string {
+	hex := "#"
+	hex += to16(r)
+	hex += to16(g)
+	hex += to16(b)
+	if a < 100 {
+		hex += to16(a * 2.55)
+	}
+	return hex
+}
+
+func rgb2hsb(r, g, b float64) (h, s, hb float64) {
+	rgb := []float64{r, g, b}
+	sort.Float64s(rgb)
+	max := rgb[2]
+	min := rgb[0]
+
+	hb = max / 255
+	if max == 0 {
+		s = 0
+	} else {
+		s = (max - min) / max
+	}
+	if r != g || r != b {
+		if max == r && g >= b {
+			h = (g-b)*60.0/(max-min) + 0.0
+		} else if max == r && g < b {
+			h = (g-b)*60.0/(max-min) + 360.0
+		} else if max == g {
+			h = (b-r)*60.0/(max-min) + 120.0
+		} else if max == b {
+			h = (r-g)*60.0/(max-min) + 240.0
+		}
+	}
+	return
+}
+
+func hsb2rgb(h, s, hb float64) (r, g, b float64) {
+	i := int64(h/60) % 6
+	f := h/60 - float64(i)
+	p := hb * (1 - s)
+	q := hb * (1 - f*s)
+	t := hb * (1 - (1-f)*s)
+	switch i {
+	case 0:
+		r = hb
+		g = t
+		b = p
+	case 1:
+		r = q
+		g = hb
+		b = p
+	case 2:
+		r = p
+		g = hb
+		b = t
+	case 3:
+		r = p
+		g = q
+		b = hb
+	case 4:
+		r = t
+		g = p
+		b = hb
+	case 5:
+		r = hb
+		g = p
+		b = q
+	}
+	r *= 255
+	g *= 255
+	b *= 255
+	return
 }
