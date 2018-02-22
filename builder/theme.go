34032830c3e81cb807f53d061f8270d845242155
ONEO <oneo.me@outlook.com>
2018-02-22 22:46:37 +0800

    完善主题生成器

--- builder/theme.go
+++ builder/theme.go
@@ -2,79 +2,147 @@ package main
 
 import (
 	"path"
-	"strings"
 
+	. "./color"
 	"./file"
 	"./vscode"
 )
 
-// Colors 颜色
-type Colors struct {
-	Background     string
-	Foreground     string
-	Border         string
-	Shadow         string
-	CodeForeground string
+// Theme 主题
+type Theme struct {
+	File        string
+	Colors      map[string]*Color
+	TokenColors map[string]*Color
 }
 
-// GenTheme 生成主题
-func GenTheme(filename string, dark bool, colors Colors) {
-	// 每次编译都要抓取 VSCode 默认的代码配色
-	// 为什么抓取默认的？
-	// 自己一个个实现很麻烦，VSCode 默认的配色已经很完美了
-	// 抓取的也只是配色方案，实际颜色也可以通过很简单的方式给替换掉
-	var defTheme *vscode.Theme
-	if dark {
-		defTheme = vscode.GetDefaultDarkTheme()
-	} else {
-		defTheme = vscode.GetDefaultLightTheme()
-	}
-	templateTheme := vscode.LoadTheme(file.Abs("./other/theme.json"))
-	// 获取默认配色
-	templateTheme.TokenColors = defTheme.TokenColors
-	// 替换界面颜色
-	for k, v := range templateTheme.Colors {
-		// 此处代码应该更美
-		// 此处应该添加更多内容
-		nv := v
-		nv = strings.Replace(nv, "#1C1F26", colors.Background, -1)
-		nv = strings.Replace(nv, "#8991A5", colors.Foreground, -1)
-
-		// 想个办法计算出这个颜色，这个颜色实际计算方式（背景 + 前景15%透明度）即为边框颜色（因为有几个地方不能用透明的，所以需要一个实色的）
-		nv = strings.Replace(nv, "#2C3039", colors.Border, -1)
-
-		nv = strings.Replace(nv, "#00000026", colors.Shadow, -1)
-		nv = strings.Replace(nv, "#D5D5D5", colors.CodeForeground, -1)
-		templateTheme.Colors[k] = nv
-	}
-	// 替换代码颜色
-	for _, v := range templateTheme.TokenColors {
-		nv := v.Settings.Foreground
-		if dark {
-			// 未实现，暂时不管
-		} else {
-			// 未实现，暂时不管
+// HasUIColor 存在这个 UI 颜色
+func (t Theme) HasUIColor(color string) bool {
+	for k := range t.Colors {
+		if k == color {
+			return true
 		}
-		v.Settings.Foreground = nv
 	}
-	// 保存
-	file.SaveJSONFile(templateTheme, path.Join(file.Abs("./themes"), filename))
+	return false
 }
 
-// GenThemes 生成全部主题
+// HasCodeColor 存在这个 Code 颜色
+func (t Theme) HasCodeColor(color string) bool {
+	for k := range t.TokenColors {
+		if k == color {
+			return true
+		}
+	}
+	return false
+}
+
+// Themes 全部主题
+var Themes = []Theme{
+	Theme{
+		File: "dark.json",
+		Colors: map[string]*Color{
+			"背景颜色":        HEX("1C1F26"),
+			"文本颜色":        HEX("8991A5"),
+			"文本颜色_次要":     HEX("8991A5").Opacity(50),
+			"边框颜色":        HEX("8991A5").Opacity(20),
+			"边框颜色_不透明":    HEX("1C1F26").AlphaBlend(HEX("8991A5").Opacity(20)),
+			"选择文本背景颜色_选择": HEX("8991A5").Opacity(30),
+			"链接颜色":        HEX("54D0FF"),
+			"正确颜色":        HEX("1FA032"),
+			"正确颜色_透明":     HEX("1FA032").Opacity(15),
+			"错误颜色":        HEX("D24741"),
+			"错误颜色_透明":     HEX("D24741").Opacity(15),
+			"警告颜色":        HEX("D29C23"),
+			"信息颜色":        HEX("4985BD"),
+			"信息文本颜色":      HEX("FFFFFF"),
+			"选项卡移入背景颜色":   HEX("8991A5").Opacity(4),
+			"选项卡选中背景颜色":   HEX("8991A5").Opacity(7),
+			"阴影颜色":        HEX("000000").Opacity(10),
+			"文本颜色_代码":     HEX("D4D4D4"),
+		},
+		TokenColors: map[string]*Color{
+			"#D4D4D4": HEX("D4D4D4"),
+			"#608B4E": HEX("608B4E"),
+			"#C586C0": HEX("C586C0"),
+			"#4EC9B0": HEX("4EC9B0"),
+			"#CE9178": HEX("CE9178"),
+			"#B5CEA8": HEX("B5CEA8"),
+			"#D7BA7D": HEX("D7BA7D"),
+			"#808080": HEX("D4D4D4").Opacity(50),
+			"#D16969": HEX("D16969"),
+			"#6796E6": HEX("569CD6"),
+			"#DCDCAA": HEX("DCDCAA"),
+			"#569CD6": HEX("569CD6"),
+			"#9CDCFE": HEX("9CDCFE"),
+			"#000080": HEX("000080"),
+			"#646695": HEX("646695"),
+			"#F44747": HEX("F44747"),
+		},
+	},
+	Theme{
+		File: "light.json",
+		Colors: map[string]*Color{
+			"背景颜色":        HEX("FFFFFF"),
+			"文本颜色":        HEX("5F6573"),
+			"文本颜色_次要":     HEX("5F6573").Opacity(50),
+			"边框颜色":        HEX("5F6573").Opacity(20),
+			"边框颜色_不透明":    HEX("FFFFFF").AlphaBlend(HEX("5F6573").Opacity(20)),
+			"选择文本背景颜色_选择": HEX("5F6573").Opacity(30),
+			"链接颜色":        HEX("109FD7"),
+			"正确颜色":        HEX("1FA032"),
+			"正确颜色_透明":     HEX("1FA032").Opacity(15),
+			"错误颜色":        HEX("D24741"),
+			"错误颜色_透明":     HEX("D24741").Opacity(15),
+			"警告颜色":        HEX("D29C23"),
+			"信息颜色":        HEX("4985BD"),
+			"信息文本颜色":      HEX("FFFFFF"),
+			"选项卡移入背景颜色":   HEX("5F6573").Opacity(4),
+			"选项卡选中背景颜色":   HEX("5F6573").Opacity(7),
+			"阴影颜色":        HEX("000000").Opacity(10),
+			"文本颜色_代码":     HEX("333333"),
+		},
+		TokenColors: map[string]*Color{
+			"#D4D4D4": HEX("333333"),
+			"#608B4E": HEX("2B6E0F"),
+			"#C586C0": HEX("9A4293"),
+			"#4EC9B0": HEX("119479"),
+			"#CE9178": HEX("A65736"),
+			"#B5CEA8": HEX("4E8D2D"),
+			"#D7BA7D": HEX("AC7B15"),
+			"#808080": HEX("333333").Opacity(50),
+			"#D16969": HEX("972424"),
+			"#6796E6": HEX("1F6096"),
+			"#DCDCAA": HEX("8B8B13"),
+			"#569CD6": HEX("1F6096"),
+			"#9CDCFE": HEX("137DB5"),
+			"#000080": HEX("000080"),
+			"#646695": HEX("646695"),
+			"#F44747": HEX("F44747"),
+		},
+	},
+}
+
+// GenThemes 生成主题
 func GenThemes() {
-	GenTheme("dark.json", true, Colors{
-		Background:     "#1C1F26",
-		Foreground:     "#8991A5",
-		Border:         "#2C3039",
-		Shadow:         "#00000026",
-		CodeForeground: "#D5D5D5",
-	})
-	GenTheme("light.json", false, Colors{
-		Background:     "#ffffff",
-		Foreground:     "#333333",
-		Border:         "#F6F6F6",
-		Shadow:         "#00000026",
-		CodeForeground: "#333333",
-	})
+	for _, theme := range Themes {
+		// 模版
+		templateTheme := vscode.LoadTheme(file.Abs("./other/theme.json"))
+		templateTheme.TokenColors = vscode.GetDefaultDarkTheme().TokenColors
+		// 修改
+		for k, v := range theme.Colors {
+			for tk, tv := range templateTheme.Colors {
+				if tv == k {
+					templateTheme.Colors[tk] = v.HEX()
+				}
+			}
+		}
+		for k, v := range theme.TokenColors {
+			for _, tv := range templateTheme.TokenColors {
+				if tv.Settings.Foreground == k {
+					tv.Settings.Foreground = v.HEX()
+				}
+			}
+		}
+		// 保存
+		file.SaveJSONFile(templateTheme, path.Join(file.Abs("./themes"), theme.File))
+	}
 }
