0e16f69a39585e82d26ccebf50f2b7847280e5ba
ONEO <oneo.me@outlook.com>
2019-09-07 12:04:06 +0800

    生成 Light 缩略图

--- builder/preview/preview.go
+++ builder/preview/preview.go
@@ -9,7 +9,6 @@ import (
 	"main/builder/old/app"
 	"math"
 	"os"
-	"path"
 	"path/filepath"
 )
 
@@ -25,44 +24,49 @@ func GenPreview() {
 			continue
 		}
 
-		previewName := "preview_" + info.Name() + ".png"
-		previewFile := filepath.Join(previews, previewName)
+		genPreview2File(filepath.Join(dir, info.Name()), filepath.Join(previews, "preview_"+info.Name()+".png"))
+		lightIcon, err := os.Stat(filepath.Join(dir, info.Name(), "light"))
+		if err == nil && lightIcon.IsDir() {
+			genPreview2File(filepath.Join(dir, info.Name(), "light"), filepath.Join(previews, "preview_"+info.Name()+"_light.png"))
+		}
+	}
+}
 
-		files, err := ioutil.ReadDir(filepath.Join(dir, info.Name()))
-		log.Error(err)
+func genPreview2File(dir, outFile string) {
+	files, err := ioutil.ReadDir(dir)
+	log.Error(err)
 
-		fileCount := len(files)
+	fileCount := len(files)
 
-		num := 15
-		s := 6
-		width := 36 * s
-		height := 24 * s
-		space := 6 * s
+	num := 15
+	s := 6
+	width := 36 * s
+	height := 24 * s
+	space := 6 * s
 
-		react := image.Rect(0, 0, num*width+space, int(math.Ceil(float64(fileCount)/float64(num)))*(height+space)+space)
-		img := image.NewRGBA(react)
+	react := image.Rect(0, 0, num*width+space, int(math.Ceil(float64(fileCount)/float64(num)))*(height+space)+space)
+	img := image.NewRGBA(react)
 
-		write, err := os.Create(previewFile)
-		log.Error(err)
+	write, err := os.Create(outFile)
+	log.Error(err)
 
-		for i, file := range files {
-			if file.IsDir() {
-				continue
-			}
-
-			line := i / num
-			index := i % num
-			x := index * width
-			y := line*(height+space) + space
-			read, err := os.Open(path.Join(dir, info.Name(), file.Name()))
-			log.Error(err)
-			icon, _, err := image.Decode(read)
-			log.Error(err)
-			read.Close()
-			draw.Draw(img, image.Rectangle{image.Point{x, y}, image.Point{x + width, y + height}}, icon, image.Point{0, 0}, draw.Src)
+	for i, file := range files {
+		if file.IsDir() {
+			continue
 		}
 
-		png.Encode(write, img)
-		write.Close()
+		line := i / num
+		index := i % num
+		x := index * width
+		y := line*(height+space) + space
+		read, err := os.Open(filepath.Join(dir, file.Name()))
+		log.Error(err)
+		icon, _, err := image.Decode(read)
+		log.Error(err)
+		read.Close()
+		draw.Draw(img, image.Rectangle{image.Point{x, y}, image.Point{x + width, y + height}}, icon, image.Point{0, 0}, draw.Src)
 	}
+
+	png.Encode(write, img)
+	write.Close()
 }
